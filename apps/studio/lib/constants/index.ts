7888baa85cd70770664855692715d35fe4dfa46c
Nikhil Saraf <nsaraf98@gmail.com>
2025-01-02 20:24:38 +0530

    add /_/studio subpath to studio
    fix docker credentials

--- apps/studio/lib/constants/index.ts
+++ apps/studio/lib/constants/index.ts
@@ -9,7 +9,7 @@ export const API_URL = (() => {
   //  If running in platform, use API_URL from the env var
   if (IS_PLATFORM) return process.env.NEXT_PUBLIC_API_URL!
   // If running in browser, let it add the host
-  if (typeof window !== 'undefined') return '/api'
+  if (typeof window !== 'undefined') return process.env.NEXT_PUBLIC_BASE_PATH + '/api'
   // If running self-hosted Vercel preview, use VERCEL_URL
   if (!!process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}/api`
   // If running on self-hosted, use NEXT_PUBLIC_SITE_URL
@@ -20,6 +20,7 @@ export const API_URL = (() => {
 export const PG_META_URL = IS_PLATFORM
   ? process.env.PLATFORM_PG_META_URL
   : process.env.STUDIO_PG_META_URL
+  
 export const BASE_PATH = process.env.NEXT_PUBLIC_BASE_PATH ?? ''
 
 /**
